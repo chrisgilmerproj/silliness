6ecfbaf8a8c582e1496974b87214768cb3124c86
Chris Gilmer <chris.gilmer@gmail.com>
2012-02-17 10:10:35 -0800

    Added docstrings and comments.  Moved constants around and cleaned up some of the code

--- card_games/war.py
+++ card_games/war.py
@@ -3,8 +3,28 @@
 import itertools
 import random
 
+# Constants used for the game
+MAX_TURNS = 5000
+SUIT_LIST = ('spade', 'diamond', 'club', 'heart')
+RANK_DICT = {
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+        11: 'J',
+        12: 'Q',
+        13: 'K',
+        14: 'A',
+        }
+
 
 class Card(object):
+    """ A Card represents a suit, rank and value """
 
     def __init__(self, rank, suit):
         self.value, self.rank = rank  # Tuple, value and representation
@@ -24,6 +44,7 @@ class Card(object):
 
 
 class Deck(object):
+    """ A Deck represents a set of cards that can be played in a game """
 
     def __init__(self, rank_dict, suit_list):
         self.rank_dict = rank_dict
@@ -32,18 +53,27 @@ class Deck(object):
         self.hands = []
 
     def __len__(self):
+        """ Return the size of the deck """
         return len(self.cards)
 
     def populate(self):
+        """ Populate the deck using the rank and suit information """
         rank_list = [(num, repr) for num, repr in self.rank_dict.iteritems()]
         self.cards = [Card(*card) for card in itertools.product(rank_list, self.suit_list)]
 
     def shuffle(self):
+        """ Shuffle the cards in the deck """
         random.shuffle(self.cards)
 
     def deal(self, num_players, deal_even=True):
+        """
+        Deal the cards into several hands based upon number of players.
+
+        @param num_players: The number of players to deal to
+        @param deal_even: When set to false players will be delt all cards
+            but the size of their hands may differ
+        """
         if deal_even and len(self.cards) % num_players != 0:
-            print deal_even, len(self.cards) % num_players != 0
             raise Exception('With %d players you will not deal evenly' % num_players)
         hands = []
         for i, card in enumerate(self.cards):
@@ -52,25 +82,6 @@ class Deck(object):
                 hands.append([])
             hands[hand_len].append(card)
         self.hands = hands
-        return self.hands
-
-
-SUIT_LIST = ('spade', 'diamond', 'club', 'heart')
-RANK_DICT = {
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-        7: '7',
-        8: '8',
-        9: '9',
-        10: '10',
-        11: 'J',
-        12: 'Q',
-        13: 'K',
-        14: 'A',
-        }
 
 
 def main():
@@ -80,7 +91,7 @@ def main():
     deck = Deck(RANK_DICT, SUIT_LIST)
     deck.populate()
     deck.shuffle()
-    deck.deal(2, deal_even=False)
+    deck.deal(2)
 
     # Play the game!
     num_turns = 0
@@ -92,7 +103,7 @@ def main():
 
         num_turns += 1
         print [len(hand) for hand in deck.hands]
-        if num_turns > 10000:
+        if num_turns > MAX_TURNS:
             print "Too many turns, quitting game"
             break
 
@@ -105,6 +116,12 @@ def main():
 
 
 def compare_cards(deck1, deck2):
+    """
+    Compare cards from the deck
+
+    This method takes two decks and returns a winner
+    along with a pile of cards the winner gets to keep
+    """
     c1 = deck1.pop(0)
     c2 = deck2.pop(0)
 
@@ -117,18 +134,24 @@ def compare_cards(deck1, deck2):
     if c1 == c2:
         print '\tWar!'
         for deck in (deck1, deck2):
-            if len(deck):
-                pile.extend(war(deck))
+            pile.extend(war_pile(deck))
+
+        # Always compare at least the last exposed card
         if len(deck1) == 0:
             deck1.append(c1)
         if len(deck2) == 0:
             deck2.append(c2)
+
+        # Compare the cards and see who wins
         winner, pile2 = compare_cards(deck1, deck2)
         pile.extend(pile2)
+
+    # Return the winner and the pile
     return winner, list(set(pile))
 
 
-def war(deck):
+def war_pile(deck):
+    """ Get up to 3 cards from the deck """
     pile = []
     for i in range(0, 3):
         if len(deck) > 1:
