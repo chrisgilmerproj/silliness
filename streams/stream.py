ef3937d86beaf51abe39cfc35fe0d7381795b660
Chris Gilmer <chris.gilmer@gmail.com>
2012-06-09 09:07:09 -0700

    Building out different streams

--- streams/stream.py
+++ streams/stream.py
@@ -1,14 +1,41 @@
 #! /usr/local/bin/python
 
+import random
+
 
 class Stream(object):
 
+    def generator(self):
+        raise NotImplementedError
+
     def popNext(self):
-        pass
+        return self.generator().next()
+
+
+class RandomStream(Stream):
+
+    def generator(self):
+        yield random.random()
+
+
+class PrimeNumberStream(Stream):
+
+    def generator(self):
+        yield 1
+
+
+class PrimeFactorStream(Stream):
+
+    def generator(self):
+        yield 1
 
 
 def main():
-    pass
+    #s = RandomStream()
+    s = PrimeNumberStream()
+    #s = PrimeFactorStream()
+    for i in xrange(5):
+        print s.popNext()
 
 
 if __name__ == "__main__":
