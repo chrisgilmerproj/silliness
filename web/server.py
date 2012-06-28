b807a7495053a64e96f6d5bc7069502e724195fb
Chris Gilmer <chris.gilmer@gmail.com>
2012-06-28 09:59:32 -0700

    Update to serve files.  Also added test directories and files

--- web/server.py
+++ web/server.py
@@ -25,19 +25,22 @@ def get_body(path):
     cur_dir = os.getcwd()
     full_dir = os.path.abspath(os.path.join(cur_dir, path))
 
-    body = ''
-    if full_dir != cur_dir:
-        body += '<li><a href="/{0}">. .</a></li>'.format('/'.join(path.split('/')[:-1]))
+    if os.path.isdir(full_dir):
+        body = ''
+        if full_dir != cur_dir:
+            body += '<li><a href="/{0}">. .</a></li>'.format('/'.join(path.split('/')[:-1]))
 
-    contents = os.listdir(full_dir)
-    contents.sort()
-    for item in contents:
-        if item[0] != '.':
-            body += '<li><a href="/{0}">{1}</a></li>'.format(os.path.join(path, item), item)
+        contents = os.listdir(full_dir)
+        contents.sort()
+        for item in contents:
+            if item[0] != '.':
+                body += '<li><a href="/{0}">{1}</a></li>'.format(os.path.join(path, item), item)
 
-    body = "<ul>{0}</ul>".format(body)
-    body = "<!DOCTYPE html><html><body>{0}</body></html>".format(body)
-    return body
+        body = "<ul>{0}</ul>".format(body)
+        body = "<!DOCTYPE html><html><body>{0}</body></html>".format(body)
+        return body
+    elif os.path.isfile(full_dir):
+        return open(full_dir, 'rb').read()
 
 def main():
     # Create Socket
