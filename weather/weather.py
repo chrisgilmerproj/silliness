ff2c8b0146c02726bc6ec96307531bf01d06a55d
Chris Gilmer <chris.gilmer@gmail.com>
2012-07-08 21:03:11 -0700

    Updated the program to look through the csv file of avg weather for cities

--- weather/weather.py
+++ weather/weather.py
@@ -1,5 +1,9 @@
 #! /usr/local/bin/python
 
+import calendar
+import csv
+import datetime
+import sys
 import urllib
 import xml.dom.minidom
 from xml.parsers.expat import ExpatError
@@ -8,7 +12,33 @@ from xml.parsers.expat import ExpatError
 class Weather(object):
     API_URL = 'http://www.google.com/ig/api'
 
-    def current(self, query):
+    avg_weather = {}
+
+    def __init__(self, filename):
+        self.current_month = calendar.month_name[datetime.datetime.now().month]
+
+        weather_dict = csv.DictReader(open(filename, 'rb'), delimiter=',')
+        for item in weather_dict:
+            city = item['City']
+            month = item['Month'].lower()
+            temp = float(item['Temp'])
+            if city not in self.avg_weather:
+                self.avg_weather[city] = {}
+            self.avg_weather[city][month] = temp
+
+    def print_cities(self):
+        for city in self.avg_weather:
+            month = self.current_month.lower()
+            avg_temp = self.avg_weather[city][month]
+            cur_temp = self.get_temp(city)
+
+            condition = 'Normal'
+            if abs(avg_temp - cur_temp) > 5.0:
+                condition = 'Unusual'
+
+            print "City: {0}, Current Month: {1}, Avg Temp {2}, Cur. Temp {3}, Condition: {4}".format(city, self.current_month, avg_temp, cur_temp, condition)
+
+    def get_temp(self, query):
         response = urllib.urlopen('{0}?weather={1}'.format(self.API_URL, urllib.quote_plus(query))).read()
 
         try:
@@ -19,7 +49,7 @@ class Weather(object):
         conditions = self.get_element_from_dom(dom, 'current_conditions')
         temperature = self.get_element_from_dom(conditions, 'temp_f')
 
-        return temperature.getAttribute('data')
+        return float(temperature.getAttribute('data'))
 
     def get_element_from_dom(self, dom, element_name):
         try:
@@ -30,14 +60,9 @@ class Weather(object):
 
 def main():
 
-    weather = Weather()
-    query = '94607'
-
-    try:
-        conditions = weather.current(query)
-        print query, conditions
-    except Exception as e:
-        sys.exit(1)
+    filename = 'weather.csv'
+    weather = Weather(filename)
+    weather.print_cities()
 
 
 if __name__ == '__main__':
