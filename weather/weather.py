4ba8eac0f753651d275a569baaaa98685030b515
Chris Gilmer <chris.gilmer@gmail.com>
2012-07-09 08:10:02 -0700

    Cleanup and abstractions to weather program

--- weather/weather.py
+++ weather/weather.py
@@ -13,31 +13,42 @@ class Weather(object):
     API_URL = 'http://www.google.com/ig/api'
 
     def __init__(self, filename):
-        self.current_month = calendar.month_name[datetime.datetime.now().month]
         self.avg_weather = {}
+        self.parse_data_from_file(filename)
 
+    def parse_data_from_file(self, filename):
         weather_dict = csv.DictReader(open(filename, 'rb'), delimiter=',')
         for item in weather_dict:
             city = item['City']
-            month = item['Month']
+            month = item['Month'].capitalize()
             temp = float(item['Temp'])
             if city not in self.avg_weather:
                 self.avg_weather[city] = {}
             self.avg_weather[city][month] = temp
 
     def print_cities(self):
+        month = self.get_month_current()
+
         for city in self.avg_weather:
-            month = self.current_month
-            avg_temp = self.avg_weather[city][month]
-            cur_temp = self.get_temp(city)
+            avg_temp = self.get_temp_average(city, month)
+            cur_temp = self.get_temp_current(city)
 
             condition = 'Normal'
             if abs(avg_temp - cur_temp) > 5.0:
                 condition = 'Unusual'
 
-            print "City: {0}, Current Month: {1}, Avg Temp {2}, Cur. Temp {3}, Condition: {4}".format(city, self.current_month, avg_temp, cur_temp, condition)
+            print "City: {0}, Current Month: {1}, Avg Temp {2}, Cur. Temp {3}, Condition: {4}".format(city, month, avg_temp, cur_temp, condition)
 
-    def get_temp(self, query):
+    def get_month_current(self):
+        return calendar.month_name[datetime.datetime.now().month]
+
+    def get_temp_average(self, city, month):
+        try:
+            return self.avg_weather[city][month]
+        except KeyError:
+            raise Exception('City "{0}" or Month "{1}" do not exist'.format(city, month))
+
+    def get_temp_current(self, query):
         response = urllib.urlopen('{0}?weather={1}'.format(self.API_URL, urllib.quote_plus(query))).read()
 
         try:
