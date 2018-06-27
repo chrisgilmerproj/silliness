c5c3d56e94410b1a987acb2cb010dafc3ba2e6df
Chris Gilmer <chris.gilmer@gmail.com>
2018-06-26 21:25:46 -0700

    Skip aliases. Don't redownload. Print errors.  Add comments

--- slack_tools/emoji_download.py
+++ slack_tools/emoji_download.py
@@ -9,15 +9,26 @@ import requests
 def main():
     slack_token = os.environ.get('SLACK_TOKEN')
     r = requests.get("https://slack.com/api/emoji.list?token={}".format(slack_token))
+
+    # Don't continue if bad request
     if not r.ok:
         sys.exit(1)
+
     for emoji_name, emoji_url in r.json()['emoji'].items():
+        # Skip any emoji records that are aliased
+        if emoji_url.startswith('alias:'):
+            continue
+
         try:
-            image_r = requests.get(emoji_url, allow_redirects=True)
             image_name = "{}{}".format(emoji_name, os.path.splitext(emoji_url)[1])
-            print(image_name)
-            open(os.path.join('./img', image_name), 'wb').write(image_r.content)
+            img_loc = os.path.join('./img', image_name)
+            # Don't re-download
+            if not os.path.isfile(img_loc):
+                print(image_name, emoji_url)
+                image_r = requests.get(emoji_url, allow_redirects=True)
+                open(img_loc, 'wb').write(image_r.content)
         except Exception:
+            print("Error: Could not parse {} from URL {}".format(emoji_name, emoji_url))
             continue
 
 
