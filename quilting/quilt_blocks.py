282fe2530d32e7b5a8958fd62f6fe8476a836592
Chris Gilmer <chris.gilmer@gmail.com>
2018-12-04 21:50:50 -0800

    Make script easier to read

--- quilting/quilt_blocks.py
+++ quilting/quilt_blocks.py
@@ -5,16 +5,16 @@ import random
 
 def main():
     colors = (
-        "color 1",
-        "color 2",
-        "color 3",
-        "color 4",
-        "color 5",
-        "color 6",
-        "color 7",
-        "color 8",
-        "color 9",
-        "color 10",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
     )
 
     block = []
