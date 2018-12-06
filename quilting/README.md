6d8601ad499557649ebfb0244ff1f7f41feb5aea
Chris Gilmer <chris.gilmer@gmail.com>
2018-12-06 12:10:32 -0800

    Clean up code, weight colors by the order they are in, and ensure triplicates don't happen.

--- quilting/README.md
+++ quilting/README.md
@@ -1,5 +1,5 @@
 # Instructions
 
 ```sh
-while true; do ./quilt_blocks.py ; read -p "Press enter to continue"; done
+./quilt_blocks.py
 ```
--- quilting/quilt_blocks.py
+++ quilting/quilt_blocks.py
@@ -4,26 +4,22 @@ import random
 
 
 def main():
-    colors = (
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-    )
+    num_colors = 6
+    colors = "123456789A"
 
+    color_start = random.randint(0, num_colors)
+    # Double the colors so we don't have to wrap
+    colors *= 2
     block = []
-    while len(block) != 6:
-        new_color = random.choice(colors)
-        if len(block) == 0 or block[-1] != new_color:
+    while len(block) != num_colors:
+        new_color = random.choice(colors[color_start:color_start + num_colors])
+        # Add as long as next color is not the same and there's not already two of them
+        if len(block) == 0 or (block[-1] != new_color and block.count(new_color) < 2):
             block.append(new_color)
-    print(block)
+    print("".join(block))
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        input("Press enter to continue")
