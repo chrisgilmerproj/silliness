7b610cdb497e44fa0f832f7c4e71f520685c243a
Chris Gilmer <chris.gilmer@gmail.com>
2020-09-02 16:24:12 -0700

    Minimal error handling

--- rust_projects/csvtutor/src/main.rs
+++ rust_projects/csvtutor/src/main.rs
@@ -1,20 +1,21 @@
 //tutorial-setup-01.rs
-// This makes the csv crate accessible to your program
 extern crate csv;
 
-// Import the standard library's I/O module so we can read from stdin.
 use std::io;
+use std::process;
 
-// The `main` function is where your program starts executing.
 fn main() {
-    // Create a CSV parser that reads data from stdin.
     let mut rdr = csv::Reader::from_reader(io::stdin());
-    // Loop over each record.
     for result in rdr.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-        // We will make this more friendly later!
-        let record = result.expect("a CSV record");
-        // Print a debug version of the record.
-        println!("{:?}", record);
+        // Examine our Result.
+        // If there was no problem, print the record.
+        // Otherwise, print the error message and quit the program.
+        match result {
+            Ok(record) => println!("{:?}", record),
+            Err(err) => {
+                println!("error reading CSV from <stdin>: {}", err);
+                process::exit(1);
+            }
+        }
     }
 }
