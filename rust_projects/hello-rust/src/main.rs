550c3109b509e73b3ee36f2e6b560f90a1e32bc4
Chris Gilmer <chris.gilmer@gmail.com>
2020-09-02 14:30:27 -0700

    Trying out VSCode

--- rust_projects/hello-rust/src/main.rs
+++ rust_projects/hello-rust/src/main.rs
@@ -9,3 +9,8 @@ fn main() {
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
 }
+
+#[test]
+fn should_fail() {
+    unimplemented!();
+}
