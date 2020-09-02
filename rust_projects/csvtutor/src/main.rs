dfa2f4fd0e9c31c61c8eb4d7de5cf3f28eac9d9e
Chris Gilmer <chris.gilmer@gmail.com>
2020-09-02 16:28:03 -0700

    Split main function and return error

--- rust_projects/csvtutor/src/main.rs
+++ rust_projects/csvtutor/src/main.rs
@@ -1,21 +1,27 @@
 //tutorial-setup-01.rs
 extern crate csv;
 
+use std::error::Error;
 use std::io;
 use std::process;
 
 fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.records() {
         // Examine our Result.
         // If there was no problem, print the record.
         // Otherwise, print the error message and quit the program.
         match result {
+            Err(err) => return Err(From::from(err)),
             Ok(record) => println!("{:?}", record),
-            Err(err) => {
-                println!("error reading CSV from <stdin>: {}", err);
-                process::exit(1);
-            }
         }
     }
+    Ok(())
 }
