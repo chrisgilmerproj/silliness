afc5fa185ad67f5ab90fa2ef507ffed846b5f6ba
Chris Gilmer <cgilmer@lucasfilm.com>
2023-08-02 10:24:45 -0700

    Don't open with file context manager. Allow two letter words.

--- stupid_words/shorten.py
+++ stupid_words/shorten.py
@@ -1,17 +1,21 @@
 #! /usr/bin/env python3
 
-from collections import defaultdict, Counter
+from collections import Counter
 
 
 def main():
-    s = defaultdict(int)
-    with open("popular.txt") as f:
-        s = Counter(
+    s = Counter(
+        map(
+            lambda w: f"{w[0]}{len(w)-2}{w[-1]}",
             map(
-                lambda w: f"{w[0]}{len(w)-2}{w[-1]}",
-                map(str.strip, filter(lambda x: len(x) > 2, f)),
-            )
+                str.strip,
+                filter(
+                    lambda x: len(x) > 1,
+                    open("popular.txt"),
+                ),
+            ),
         )
+    )
 
     for item in sorted(s.items(), key=lambda x: x[1], reverse=True)[:20]:
         print(item)
