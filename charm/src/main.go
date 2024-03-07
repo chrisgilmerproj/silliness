3ebf0c4ecfb06b841f1c9bd8e81dac5235b8a2b1
Chris Gilmer <chris.gilmer@gmail.com>
2024-03-07 13:37:00 -0800

    Add cobra/viper/pflag

--- charm/src/main.go
+++ charm/src/main.go
@@ -2,181 +2,65 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"sort"
 	"strings"
 
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-const defaultListHeight = 14
-const defaultListWidth = 20
-
-var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	docStyle          = lipgloss.NewStyle().Margin(1, 2)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+const (
+	cliName     = "ssmpicker"
+	ver         = "v0.0.1"
+	flagVerbose = "verbose"
 )
 
-type item string
-
-func (i item) FilterValue() string { return "" }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int  { return 1 }
-func (d itemDelegate) Spacing() int { return 0 }
-func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
-	return nil
-}
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
+func initViper(cmd *cobra.Command) (*viper.Viper, error) {
+	v := viper.New()
+	errBind := v.BindPFlags(cmd.Flags())
+	if errBind != nil {
+		return v, fmt.Errorf("error binding flag set to viper: %w", errBind)
 	}
-
-	str := fmt.Sprintf("%d. %s", index+1, i)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
-
-type model struct {
-	list        list.Model
-	quitting    bool
-	groupedData GroupedKeyValueData
-	key         string
-	val         string
-	instanceId  string
-}
-
-func (m model) Init() tea.Cmd {
-	return m.UpdateList()
-}
-
-func (m model) UpdateList() tea.Cmd {
-	return func() tea.Msg {
-		keyList := []string{}
-		if m.key != "" && m.val == "" {
-			for val := range m.groupedData[m.key] {
-				keyList = append(keyList, val)
-			}
-			sort.Strings(keyList)
-			ec2ValList := []list.Item{}
-			for _, val := range keyList {
-				ec2ValList = append(ec2ValList, item(val))
-			}
-			return ec2ValList
-		} else if m.key != "" && m.val != "" {
-			ec2DataList := []list.Item{}
-			for _, data := range m.groupedData[m.key][m.val] {
-				ec2DataList = append(ec2DataList, item(data.InstanceId))
-			}
-			return ec2DataList
-		}
-
-		ec2List := []list.Item{}
-		for key := range m.groupedData {
-			keyList = append(keyList, key)
-		}
-		sort.Strings(keyList)
-		for _, key := range keyList {
-			ec2List = append(ec2List, item(key))
-		}
-		return ec2List
-	}
-}
-
-func (m model) PrintCmd() string {
-	return fmt.Sprintf("aws ssm start-session --target %s", m.instanceId)
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case []list.Item:
-		m.list.SetItems([]list.Item(msg))
-		return m, nil
-
-	case tea.WindowSizeMsg:
-		h, v := docStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
-
-	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
-
-		case "enter":
-			i, ok := m.list.SelectedItem().(item)
-			if ok {
-				if len(m.key) == 0 {
-					m.key = string(i)
-					return m, m.UpdateList()
-				} else if len(m.val) == 0 {
-					m.val = string(i)
-					return m, m.UpdateList()
-				} else {
-					m.instanceId = string(i)
-					m.quitting = true
-					return m, nil
-				}
-			}
-		}
-	}
-
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	if len(m.list.Items()) == 0 {
-		return quitTextStyle.Render("Waiting for results from AWS.")
-	}
-	// if m.key != "" && m.val == "" {
-	// 	return quitTextStyle.Render(fmt.Sprintf("Looking at: %s.", m.key))
-	// }
-	// if m.key != "" && m.val != "" {
-	// 	return quitTextStyle.Render(fmt.Sprintf("Looking at: %s %s.", m.key, m.val))
-	// }
-	if m.quitting {
-		if len(m.key) != 0 && len(m.val) != 0 {
-			return quitTextStyle.Render(m.PrintCmd())
-		}
-		return quitTextStyle.Render("Done.")
-	}
-	return docStyle.Render(m.list.View())
+	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	v.SetEnvPrefix(strings.ToUpper(cliName))
+	v.AutomaticEnv()
+	return v, nil
 }
 
 func main() {
+	rootCommand := &cobra.Command{
+		Use:                   fmt.Sprintf("%s [flags]", cliName),
+		DisableFlagsInUseLine: true,
+		Short:                 fmt.Sprintf("%s is a CLI tool for picking ssm targets to log into", cliName),
+	}
 
-	l := list.New([]list.Item{}, itemDelegate{}, defaultListWidth, defaultListHeight)
-	l.Title = "EC2 Instance Tags"
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
+	ec2SearchCommand := &cobra.Command{
+		Use:                   `ec2 [flags]`,
+		DisableFlagsInUseLine: true,
+		Short:                 "search ec2 instances",
+		SilenceErrors:         true,
+		SilenceUsage:          true,
+		RunE:                  ec2Search,
+	}
+	initEc2SearchFlags(ec2SearchCommand.Flags())
 
-	m := model{list: l}
-	m.groupedData = groupEC2Instances()
+	versionCommand := &cobra.Command{
+		Use:                   `version`,
+		DisableFlagsInUseLine: true,
+		Short:                 "display version and quit",
+		SilenceErrors:         true,
+		SilenceUsage:          true,
+		RunE:                  printVersion,
+	}
 
-	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
-		fmt.Println("Error running program:", err)
+	rootCommand.AddCommand(
+		ec2SearchCommand,
+		versionCommand,
+	)
+
+	if err := rootCommand.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%s: %s\n", cliName, err.Error())
+		_, _ = fmt.Fprintf(os.Stderr, "Try '%s --help' for more information.\n", cliName)
 		os.Exit(1)
 	}
 }
