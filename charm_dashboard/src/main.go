dd536038d467a9bdbb3021e94726f5ca4cfb69ce
Chris Gilmer <chris.gilmer@gmail.com>
2024-03-08 16:14:29 -0800

    Render the command and fix filtering.

--- charm_dashboard/src/main.go
+++ charm_dashboard/src/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -33,6 +35,10 @@ var (
 			Padding(1, 2).
 			Border(lipgloss.RoundedBorder()).
 			BorderForeground(lipgloss.Color("62"))
+	commandStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("220"))
 	helpStyle = lipgloss.NewStyle().
 			Foreground(lipgloss.Color("241"))
 )
@@ -53,7 +59,6 @@ func (t Tag) Title() string {
 }
 
 func (t Tag) Description() string {
-	// return truncateString(strings.Join(t.values, ", "), 20)
 	switch t.section {
 	case instance:
 		return "instance"
@@ -72,19 +77,26 @@ func (t Tag) Values() []string {
 
 /* Main Model */
 type Model struct {
-	focused  section
-	data     GroupedKeyValueData
-	lists    []list.Model
-	loaded   bool
-	quitting bool
+	focused    section
+	data       GroupedKeyValueData
+	lists      []list.Model
+	loaded     bool
+	quitting   bool
+	instanceId string
 }
 
 func New() *Model {
 	return &Model{}
 }
 
-func (m *Model) SelectTagName() tea.Msg {
+func (m *Model) SelectListItem() tea.Msg {
 	selectedItem := m.lists[m.focused].SelectedItem()
+	// Move back a column if no items can be selected
+	if selectedItem == nil {
+		m.Prev()
+		return nil
+	}
+	// Process the selected item
 	selectedTag := selectedItem.(Tag)
 	switch selectedTag.section {
 	case tagKey:
@@ -94,13 +106,20 @@ func (m *Model) SelectTagName() tea.Msg {
 			newList = append(newList, Tag{section: tagValue, name: val, values: instances})
 		}
 		m.lists[tagValue].SetItems(newList)
+		m.lists[tagValue].ResetFilter()
 		m.lists[instance].SetItems([]list.Item{})
+		m.lists[instance].ResetFilter()
+		m.Next()
 	case tagValue:
 		newList := []list.Item{}
 		for _, val := range selectedTag.Values() {
 			newList = append(newList, Tag{section: instance, name: val, values: []string{}})
 		}
 		m.lists[instance].SetItems(newList)
+		m.lists[instance].ResetFilter()
+		m.Next()
+	case instance:
+		m.instanceId = selectedTag.Key()
 	}
 	return nil
 }
@@ -143,6 +162,7 @@ func (m *Model) initLists(width, height int) {
 	// Init Values as empty, fill this later
 	m.lists[tagValue].Title = "Key Values"
 	m.lists[tagValue].SetItems([]list.Item{})
+
 	// Init Instances as empty, fill this later
 	m.lists[instance].Title = "Instances"
 	m.lists[instance].SetItems([]list.Item{})
@@ -169,7 +189,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "right":
 			m.Next()
 		case "enter":
-			return m, m.SelectTagName
+			m.SelectListItem()
 		}
 	}
 	var cmd tea.Cmd
@@ -181,40 +201,69 @@ func (m Model) View() string {
 	if m.quitting {
 		return docStyle.Render("")
 	}
+
 	if m.loaded {
+		doc := strings.Builder{}
+
 		tagKeyView := m.lists[tagKey].View()
 		tagValueView := m.lists[tagValue].View()
 		instanceView := m.lists[instance].View()
+
+		var render string
 		switch m.focused {
 		case tagValue:
-			return docStyle.Render(lipgloss.JoinHorizontal(lipgloss.Left,
+			render = lipgloss.JoinHorizontal(lipgloss.Left,
 				columnStyle.Render(tagKeyView),
 				focusedStyle.Render(tagValueView),
 				columnStyle.Render(instanceView),
-			))
+			)
 		case instance:
-			return docStyle.Render(lipgloss.JoinHorizontal(lipgloss.Left,
+			render = lipgloss.JoinHorizontal(lipgloss.Left,
 				columnStyle.Render(tagKeyView),
 				columnStyle.Render(tagValueView),
 				focusedStyle.Render(instanceView),
-			))
+			)
 		default:
-			return docStyle.Render(lipgloss.JoinHorizontal(lipgloss.Left,
+			render = lipgloss.JoinHorizontal(lipgloss.Left,
 				focusedStyle.Render(tagKeyView),
 				columnStyle.Render(tagValueView),
 				columnStyle.Render(instanceView),
-			))
+			)
 		}
+		doc.WriteString(render)
+		if len(m.instanceId) > 0 {
+			doc.WriteString("\n\n" + commandStyle.Render(m.PrintCmd(m.instanceId, "")))
+		}
+		return docStyle.Render(doc.String())
 	} else {
 		return docStyle.Render("loading ...")
 	}
+}
 
+func (m Model) PrintCmd(instanceId, portForwarding string) string {
+	command := []string{
+		"aws",
+		"ssm",
+		"start_session",
+		"--target",
+		instanceId,
+	}
+	if len(portForwarding) > 0 {
+		portFromTo := strings.Split(portForwarding, ":")
+		ports := map[string][]string{
+			"portNumber":      {portFromTo[0]},
+			"localPortNumber": {portFromTo[1]},
+		}
+		compactPorts, _ := json.Marshal(ports)
+		command = append(command, "--document-name AWS-StartPortForwardingSession")
+		command = append(command, fmt.Sprintf("--parameters '%s'", compactPorts))
+	}
+	return strings.Join(command, " ")
 }
 
 func main() {
 	m := New()
-	//if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
-	if _, err := tea.NewProgram(m).Run(); err != nil {
+	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
